d5fbc554743f1b30d55540bab0c0bb13bead17ad
Bettina Riedl <riedl.bettina@freenet.de>
2023-06-15 11:03:41 +0200

    add KNN Neighborlist TODO! shifts when pbc = True!

--- src/schnetpack/transform/neighborlist.py
+++ src/schnetpack/transform/neighborlist.py
@@ -20,6 +20,7 @@ __all__ = [
     "WrapPositions",
     "SkinNeighborList",
     "FilterNeighbors",
+    "KNNNeighborList"
 ]
 
 import schnetpack as spk
@@ -518,6 +519,93 @@ class TorchNeighborList(NeighborListTransform):
                 torch.cartesian_prod(o, o, r3),
             ]
         )
+
+
+
+class KNNNeighborList(TorchNeighborList):
+    """
+         Returns the k-nearest Neighbors
+    """
+    def __init__(self, k):
+        super(KNNNeighborList, self).__init__(10.0)
+        self.k = k
+
+    def _build_neighbor_list(self, Z, positions, cell, pbc, cutoff):
+        # Check if shifts are needed for periodic boundary conditions
+        if torch.all(pbc == 0):
+            shifts = torch.zeros(0, 3, device=cell.device, dtype=torch.long)
+        else:
+            shifts = self._get_shifts(cell, pbc, cutoff)
+        idx_i, idx_j, offset = self._get_neighbor_pairs(positions, cell, shifts, cutoff)
+
+        # Sort along first dimension (necessary for atom-wise pooling)
+        sorted_idx = torch.argsort(idx_i)
+        idx_i = idx_i[sorted_idx]
+        idx_j = idx_j[sorted_idx]
+
+        bi_offset = torch.cat((-offset, offset), dim=0)
+        offset = bi_offset[sorted_idx]
+        offset = torch.mm(offset.to(cell.dtype), cell)
+
+        return idx_i, idx_j, offset
+
+    def _get_neighbor_pairs(self, positions, cell, shifts, cutoff):
+        """Compute pairs of atoms that are neighbors
+        Copyright 2018- Xiang Gao and other ANI developers
+        (https://github.com/aiqm/torchani/blob/master/torchani/aev.py)
+        Arguments:
+            positions (:class:`torch.Tensor`): tensor of shape
+                (molecules, atoms, 3) for atom coordinates.
+            cell (:class:`torch.Tensor`): tensor of shape (3, 3) of the three vectors
+                defining unit cell: tensor([[x1, y1, z1], [x2, y2, z2], [x3, y3, z3]])
+            shifts (:class:`torch.Tensor`): tensor of shape (?, 3) storing shifts
+        """
+        num_atoms = positions.shape[0]
+        all_atoms = torch.arange(num_atoms, device=cell.device)
+
+        # 1) Central cell
+        pi_center, pj_center = torch.combinations(all_atoms).unbind(-1)
+        shifts_center = shifts.new_zeros(pi_center.shape[0], 3)
+
+        # 2) cells with shifts
+        # shape convention (shift index, molecule index, atom index, 3)
+        num_shifts = shifts.shape[0]
+        all_shifts = torch.arange(num_shifts, device=cell.device)
+        shift_index, pi, pj = torch.cartesian_prod(
+            all_shifts, all_atoms, all_atoms
+        ).unbind(-1)
+        shifts_outside = shifts.index_select(0, shift_index)
+
+        # 3) combine results for all cells
+        shifts_all = torch.cat([shifts_center, shifts_outside])
+        pi_all = torch.cat([pi_center, pi])
+        pj_all = torch.cat([pj_center, pj])
+
+        # 4) Compute shifts and distance vectors
+        shift_values = torch.mm(shifts_all.to(cell.dtype), cell)
+        Rij_all = positions[pi_all] - positions[pj_all] + shift_values
+
+        # 5) Compute distances, and find all pairs within cutoff
+        distances = torch.norm(Rij_all, dim=1)
+        max_distance = distances.max()
+        num_all_atoms = pi_all.shape[0]
+        distance_matrix = torch.full((num_atoms, num_atoms), max_distance + 1)
+
+        distance_matrix[pi_all, pj_all] = distances
+        distance_matrix[pj_all, pi_all] = distances
+
+        distances, neighbor_pairs = torch.topk(-distance_matrix, self.k)
+        distances = - distances
+
+        # TODO shifts!
+        #offsets = #shifts_all[pair_index]
+        offsets = torch.tensor([[0, 0, 0]]).repeat(num_atoms * self.k, 1)
+
+        idx_i = all_atoms.repeat_interleave(self.k)
+        idx_j = neighbor_pairs.flatten()
+
+        return idx_i, idx_j, offsets
+
 
 
 class FilterNeighbors(Transform):
