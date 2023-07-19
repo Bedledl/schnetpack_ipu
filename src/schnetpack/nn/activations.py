1ff48d3229b7372649c0893f2a68978c4853ec77
Bettina Riedl <riedl.bettina@freenet.de>
2023-07-19 18:19:36 +0200

    replace activation with ipu-friendly version(softplus_backward is not implemnted)

--- src/schnetpack/nn/activations.py
+++ src/schnetpack/nn/activations.py
@@ -19,7 +19,10 @@ def shifted_softplus(x: torch.Tensor):
         torch.Tensor: shifted soft-plus of input.
 
     """
-    return functional.softplus(x) - math.log(2.0)
+    shift = torch.log(torch.tensor(2.0))
+    u = torch.log1p(torch.exp(-x.sign() * x))
+    v = torch.clamp_min(x, 0.0)
+    return u + v - shift
 
 
 def softplus_inverse(x: torch.Tensor):
