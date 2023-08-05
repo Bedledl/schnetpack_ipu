65dcb91a4151962a1a28683d21819d4ad9620f4b
Bettina Riedl <riedl.bettina@freenet.de>
2023-08-05 13:44:37 +0200

    avoid unnecessary NL calculation

--- src/schnetpack/ipu_modules/KNNModule.py
+++ src/schnetpack/ipu_modules/KNNModule.py
@@ -6,6 +6,7 @@ Also this module does no support batching
 from typing import Dict
 
 import torch
+import poptorch
 from schnetpack import properties
 from schnetpack.transform import Transform
 
@@ -23,27 +24,17 @@ class KNNNeighborTransform(Transform):
     if we know that the batch size says constant throughout the use of this Module, we can use the
     n_atoms and n_molecules parameter to create a buffer with constant idx_i input.
     """
-    def __init__(self, k, n_replicas, n_atoms):
+    def __init__(self, k, n_replicas, n_atoms, cutoff_shell=2.):
         super(KNNNeighborTransform, self).__init__()
         self.k = int(k)
         self.n_atoms = n_atoms
         self.n_replicas = n_replicas
+        self.cutoff_shell = cutoff_shell
+        self.register_buffer("previous_positions",
+                             torch.full((n_replicas * n_atoms, 3), float('nan'),  dtype=torch.float32))
+        self.register_buffer("previous_idx_j", torch.zeros(n_replicas * n_atoms * k, dtype=torch.int32))
 
-    def forward(
-            self,
-            inputs: Dict[str, torch.Tensor],
-    ) -> Dict[str, torch.Tensor]:
-        if inputs.get(properties.idx_i, None) is None:
-            raise ValueError("Input dictionary nor self.idx_i do not contain the idx_i value. "
-                             "This can lead to errors throughout the NN.")
-
-        if inputs.get(properties.offsets, None) is None:
-            raise ValueError("Input dictionary nor self.offsets do not contain the offsets value. "
-                             "This can lead to errors throughout the NN.")
-
-        # TODO batching! without a real batching implemnetation we mix atoms from different molecules
-        positions = inputs[properties.position]
-
+    def __calc_nl(self, positions: torch.Tensor) -> torch.Tensor:
         idx_j_batches = []
 
         offset = 0
@@ -70,5 +61,41 @@ class KNNNeighborTransform(Transform):
 
             offset += self.n_atoms
 
-        inputs[properties.idx_j] = torch.cat(idx_j_batches)
+        idx_j = torch.cat(idx_j_batches)
+
+        self.previous_positions.copy_(positions)
+        self.previous_idx_j.copy_(idx_j)
+        return idx_j
+
+    def __get_prev_idx_j(self) -> torch.Tensor:
+        return self.previous_idx_j
+
+    def forward(
+            self,
+            inputs: Dict[str, torch.Tensor],
+    ) -> Dict[str, torch.Tensor]:
+        if inputs.get(properties.idx_i, None) is None:
+            raise ValueError("Input dictionary nor self.idx_i do not contain the idx_i value. "
+                             "This can lead to errors throughout the NN.")
+
+        if inputs.get(properties.offsets, None) is None:
+            raise ValueError("Input dictionary nor self.offsets do not contain the offsets value. "
+                             "This can lead to errors throughout the NN.")
+
+        # TODO batching! without a real batching implemnetation we mix atoms from different molecules
+        positions = inputs[properties.position]
+
+        # check if calculating neighborlist is necessary:
+        first_run = self.previous_positions.isnan().sum(-1, dtype=torch.bool)
+
+        print("KNNModule: found NAN values in stored previous positions.")
+        diff = torch.pow(self.previous_positions - positions, 2).sum(-1).sqrt()
+        # TODO minimal expample with abs()?
+
+        diff_greater_shell = diff > 0.5 * self.cutoff_shell
+        nl_calculation_required = torch.any(torch.cat([first_run, diff_greater_shell]))
+        idx_j = poptorch.cond(nl_calculation_required, self.__calc_nl, [positions], self.__get_prev_idx_j, [])[0]
+
+        inputs[properties.idx_j] = idx_j
+
         return inputs
