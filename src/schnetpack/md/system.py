95409f945238e79def21a61bfa49ad2b1b137311
Bettina Riedl <riedl.bettina@freenet.de>
2023-09-11 22:48:11 +0200

    TODO masses?

--- src/schnetpack/md/system.py
+++ src/schnetpack/md/system.py
@@ -160,7 +160,7 @@ class System(UninitializedMixin, nn.Module):
 
         # 3) Construct basic property arrays
         self.atom_types = torch.zeros(self.total_n_atoms, dtype=torch.long)
-        self.masses = torch.ones(1, self.total_n_atoms, 1)
+        self.masses = torch.ones(self.n_replicas, self.total_n_atoms, 1)
 
         # Relevant for dynamic properties: positions, momenta, forces
         self.positions = torch.zeros(self.n_replicas, self.total_n_atoms, 3)
@@ -186,7 +186,7 @@ class System(UninitializedMixin, nn.Module):
             self.atom_types[idx_c : idx_c + n_atoms] = torch.from_numpy(
                 molecules[i].get_atomic_numbers()
             ).long()
-            self.masses[0, idx_c : idx_c + n_atoms, 0] = torch.from_numpy(
+            self.masses[:, idx_c : idx_c + n_atoms, 0] = torch.from_numpy(
                 molecules[i].get_masses() * mass2internal
             )
 
